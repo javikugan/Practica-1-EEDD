e9cf0f23cd53ed25ab6a8d6d94b8aeb1befa8df9
J4V1KVG4N <javikugan@gmail.com>
2022-10-25 19:47:34 +0200

    Inicial

---
+++ NodoCola.h
@@ -0,0 +1,15 @@
+#ifndef NODOCOLA_H
+#define NODOCOLA_H
+#include <Maleta.h>   
+#include <iostream>
+class NodoCola
+{
+friend class Cola;
+private:
+NodoCola *siguiente;
+char elemento;
+public:
+NodoCola();
+NodoCola(char e, NodoCola*sig = NULL);
+~NodoCola();
+};
