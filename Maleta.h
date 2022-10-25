e761be2e11344e039ba9cb3b6f916d547b8b03f6
J4V1KVG4N <javikugan@gmail.com>
2022-10-25 20:08:41 +0200

    asd

--- Maleta.h
+++ Maleta.h
@@ -1,6 +1,15 @@
 #include <iostream>
 struct Maleta
 {
+   char id[6];
+   char dni[10]; //creo dni
+   char destino[3]; 
+   char origen[3];
+   int localizador;
+   char nombre[30];
+   int hora; 
+   
+
     
     /* data */
 };
