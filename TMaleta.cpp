31f3ca62e2232da5e1f9eb72cc9897d0a74de0b6
larapolnun <lara2003rufo@gmail.com>
2022-11-01 13:14:05 +0100

    alea

--- TMaleta.cpp
+++ TMaleta.cpp
@@ -1,34 +1,21 @@
-
+#ifndef TMALETA_H
+#define TMALETA_H
 #include "TMaleta.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
 
-int generar_numero_aleatorio(int minimo, int maximo){
-    srand(time(NULL));
-    int aleat = minimo + rand()%maximo;
-    return aleat;
+TMaleta::TMaleta()
+{
+    
+
 }
-void cadena_aleatoria(int longitud, char *destino) {
-    char muestra[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-    for (int x = 0; x < longitud; x++) {
-        int indiceAleatorio = generar_numero_aleatorio(0, (int) strlen(muestra) - 1);
-        destino[x] = muestra[indiceAleatorio];
-    }
-}
-void Id_Vuelo_aleatorio(char *destino) {
-   std::string idVuelo[] = {"IB3412", "AC4832", "RY2781", "MS1032", "UA5389", "AE2334", "KL9768"};
-    int indiceAleatorio = generar_numero_aleatorio(0, (int) strlen(idVuelo) - 1);
-    destino = idVuelo[indiceAleatorio];
+TMaleta:: ~TMaleta()
+{
     
 }
 
 
-int main(){
-    char id[7];
-    Id_Vuelo_aleatorio(id);
-    printf(id);
-    return 0;
-}
+
+
+
+
+srand(time(NULL));
+    numaleat = 1 + rand() %1000;
