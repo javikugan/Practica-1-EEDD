39b9ef9beb0225bcc0e8ddf3e55683f8d32f10fb
Javier Roma <javikugan@gmail.com>
2022-11-01 12:49:06 +0100

    asdasd

--- TMaleta.cpp
+++ TMaleta.cpp
@@ -6,10 +6,10 @@
 #include <string.h>
 #include <time.h>
 
-int generar_n_aleatorio(int minimo, int maximo){
+int generar_numero_aleatorio(int minimo, int maximo){
     srand(time(NULL));
-    int aleat = minimo + rand()%maximo
-    return aleat
+    int aleat = minimo + rand()%maximo;
+    return aleat;
 }
 void cadena_aleatoria(int longitud, char *destino) {
     char muestra[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
@@ -17,6 +17,14 @@ void cadena_aleatoria(int longitud, char *destino) {
         int indiceAleatorio = generar_numero_aleatorio(0, (int) strlen(muestra) - 1);
         destino[x] = muestra[indiceAleatorio];
     }
+}
+void dni_aleatorio(char *destino){
+    char numeros[] = "0123456789"
+    for (int x = 0; x < 8; x++) {
+        int indiceAleatorio = generar_numero_aleatorio(0, (int) strlen(muestra) - 1);
+        destino[x] = muestra[indiceAleatorio];
+    
+
 }
 void Id_Vuelo_aleatorio(char *destino) {
    std::string idVuelo[] = {"IB3412", "AC4832", "RY2781", "MS1032", "UA5389", "AE2334", "KL9768"};
