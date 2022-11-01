a882e5668f00b2c0720692571286ebaa7e84a1ec
Javier Roma <javikugan@gmail.com>
2022-11-01 13:29:52 +0100

    sdad

--- TMaleta.cpp
+++ TMaleta.cpp
@@ -2,9 +2,12 @@
 #include "TMaleta.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+using namespace std;
+
 
 int generar_numero_aleatorio(int minimo, int maximo){
     srand(time(NULL));
@@ -19,17 +22,16 @@ void cadena_aleatoria(int longitud, char *destino) {
     }
 }
 
-void Id_Vuelo_aleatorio(char *destino) {
-   std::string idVuelo[] = {"IB3412", "AC4832", "RY2781", "MS1032", "UA5389", "AE2334", "KL9768"};
-    int indiceAleatorio = generar_numero_aleatorio(0, (int) strlen(idVuelo) - 1);
-    destino[0] = idVuelo[indiceAleatorio];
-    
+std::string Id_Vuelo_aleatorio() {
+    std::string idVuelo[] = {"IB3412", "AC4832", "RY2781", "MS1032", "UA5389", "AE2334", "KL9768"};
+    int indiceAleatorio = generar_numero_aleatorio(0, 7);
+    std::string destino = idVuelo[indiceAleatorio];
+    return destino;
 }
 
 
 int main(){
-    char id[7];
-    Id_Vuelo_aleatorio(id);
-    printf(id);
+    
+    cout << Id_Vuelo_aleatorio();
     return 0;
 }
