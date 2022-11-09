f87526e57e0af6d840b3479b0ea704a893923ff4
larapolnun <lara2003rufo@gmail.com>
2022-11-09 17:21:03 +0100

    gvjvjv

--- TMaleta.h
+++ TMaleta.h
@@ -6,7 +6,7 @@ typedef struct TMaleta
    char id[6];
    char dni[10]; //creo dni
    char destino[3]; 
-   char origen[3];
+   char origen[3];                                          
    int localizador;
    char nombre[30];
    int hora; };
