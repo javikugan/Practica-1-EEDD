3b2f2c4f157a13da6a56b35cd06918f3f7cf320a
J4V1KVG4N <javikugan@gmail.com>
2022-11-09 17:31:43 +0100

    SDS

--- TMaleta.cpp
+++ TMaleta.cpp
@@ -23,34 +23,34 @@ void cadena_aleatoria(int longitud, char *destino) {
 char dni_aleat(){
     int dninum;
     char dni;
-    char muestra[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string muestra[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     dninum = generar_numero_aleatorio(10000000,99999999);
     dni = int.TryParse(dninum);
     int indiceAleatorio = generar_numero_aleatorio(0, (int) strlen(muestra) - 1);
     dni[8] = muestra[indiceAleatorio];
-    return dni
+    return dni;
 
 }
 char generar_origen(){
     char origen;
-    char muestra[]= {"MADRID", "MÁLAGA", "COPENHAGUE", "LONDRES-STANSTED", "WASHINGTON DULLES", "VIRGINIA", "CHANTILLY"}
-    origen_n = generar_numero_aleatorio(0,6);
-    std::string origen = muestra[indiceAleatorio];
+    std::string muestra[]= {"MADRID", "MÁLAGA", "COPENHAGUE", "LONDRES-STANSTED", "WASHINGTON DULLES", "VIRGINIA", "CHANTILLY"}
+    int origen_n = generar_numero_aleatorio(0,6);
+    std::string origen = muestra[origen_n];
     return origen; 
 }
 
 char generar_destino(){
     char destino;
-    char muestra[]= {"ARGENTINA", "CHILE", "ALEMANIA", "BUENOS AIRES", "ISLANDIA", "ALBANIA", "BÉLGICA"}
-    destino_n = generar_numero_aleatorio(0,6)
-    std::string destino = muestra[indiceAleatorio];
+    std::string muestra[]= {"ARGENTINA", "CHILE", "ALEMANIA", "BUENOS AIRES", "ISLANDIA", "ALBANIA", "BÉLGICA"}
+    int destino_n = generar_numero_aleatorio(0,6)
+    std::string destino = muestra[destino_n];
     return destino; 
 
 }
 
 int generar_hora_salida(){
     char hora_salida;
-    char muestra[]= {"2147", "2354", "1234"}
+    std::string muestra[]= {"2147", "2354", "1234"}
     std:: string hora_salida = muestra[generar_numero_aleatorio(0,2)];
     return hora_salida;
 
@@ -65,31 +65,13 @@ std::string Id_Vuelo_aleatorio() {
     std::string destino = idVuelo[indiceAleatorio];
     return destino;
 }
-TMaleta::TMaleta(char idf, char dnif, char destinof, char origenf, int localizadorf, char nombref, int horaf){
-    id = Id_Vuelo_aleatorio();
-    dni= dni_aleat();
-    destino = destinof;
-    origen = origenf;
-    localizador = localizadorf;
-    nombre = nombref;
-    hora = horaf;
-}
 
-TMaleta crear_Maleta_rand(){
-    id = Id_Vuelo_aleatorio();
-    dni= dni_aleat();
-    destino = destinof;
-    origen = origenf;
-    localizador = localizadorf;
-    nombre = nombref;
-    hora = horaf;
-}
+
 int main(){
     std::string IdVuelo = Id_Vuelo_aleatorio();
-    printf(IdVuelo);
+    cout << IdVuelo;
     cout << "Imprime los destinos generados de forma aleatoria";
-    generar_hora_salida()
-    cout << ""
+    cout << generar_hora_salida();
     return 0;
 
 }
