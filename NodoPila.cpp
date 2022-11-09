9335334faa2f0b2d7f3b966b7c1a1c825e030a8b
J4V1KVG4N <javikugan@gmail.com>
2022-11-09 17:31:55 +0100

    asda

--- NodoPila.cpp
+++ NodoPila.cpp
@@ -1,12 +1,7 @@
 #include "NodoPila.h"
-#include <TMaleta.h>   
+#include "TMaleta.h"
 //hola
-NodoPila::NodoPila()
-{
-valor=0;
-siguiente=NULL;
-//ctor
-}
+
 NodoPila::NodoPila(int v, NodoPila *sig)
 {
 valor = v;
