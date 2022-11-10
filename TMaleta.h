f6d7abce6f9e8d30360e7979919f420ca826513d
J4V1KVG4N <javikugan@gmail.com>
2022-11-10 16:04:44 +0100

    asdasdasda

--- TMaleta.h
+++ TMaleta.h
@@ -1,12 +1,11 @@
 #include <iostream>
 struct TMaleta
 {
-   std::string id[6];
-   std::string dni[10]; //creo dni
-   std::string destino[3]; 
-   std::string origen[3];
-   int localizador; //4 digitos
-   std::string nombre[30];
-   int hora; 
+   std::string id;
+   std::string dni; //creo dni
+   std::string destino; 
+   std::string origen;
+   std::string localizador; //4 digitos
+   std::string hora; 
    /* data */
 };
