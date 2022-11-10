b6df7d9251477171830fef9bf77f8fb4886bbc85
Javier Roma <javikugan@gmail.com>
2022-11-10 01:36:10 +0100

    asda

--- TMaleta.h
+++ TMaleta.h
@@ -1,15 +1,12 @@
 #include <iostream>
 struct TMaleta
 {
-   char id[6];
-   char dni[10]; //creo dni
-   char destino[3]; 
-   char origen[3];
+   std::string id[6];
+   std::string dni[10]; //creo dni
+   std::string destino[3]; 
+   std::string origen[3];
    int localizador; //4 digitos
-   char nombre[30];
+   std::string nombre[30];
    int hora; 
-   
-
-    
-    /* data */
+   /* data */
 };
